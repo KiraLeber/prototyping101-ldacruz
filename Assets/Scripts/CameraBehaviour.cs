56d61f78560bd1f1bb0057e0027eda172e42439a
Linda Kruse <gitkraken@lindakruse.de>
2020-11-06 14:52:03 +0100

    add camera behaviour
    set camera to car position with offset

---
+++ Assets/Scripts/CameraBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour
+{
+    public GameObject car; 
+    
+    // offset from local point of car to camera position
+    private Vector3 offset = new Vector3(0.4f, 1.4f, -5.2f);
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        // set the camera to the position of the car with offset 
+        transform.position = car.transform.position + offset;
+    }
+}
