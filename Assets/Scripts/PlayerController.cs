5300d9422d2dcfecd5f51ec023fa942da78d4194
Linda Kruse <gitkraken@lindakruse.de>
2020-11-12 23:02:41 +0100

    add vertical input to player controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5; // Speed for our hot red vehicle
+    public float verticalInput; // Input of arrow keys up and down
 
     // Start is called before the first frame update
     void Start()
@@ -15,7 +16,14 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Move our hot red vehicle!
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        verticalInput = Input.GetAxis("Vertical");
+        // use horizontalInput to turn car left and right
+        
+        
+        // Move our hot red vehicle forward and backwards
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+
+        // Turn our hot red vehicle left and right
+
     }
 }
