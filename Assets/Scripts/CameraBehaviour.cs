d2966dd0d0ff89acc313e5569f8dccbc783493ed
Linda Kruse <gitkraken@lindakruse.de>
2020-11-13 11:30:32 +0100

    camera follow car with rotation
    use world space to follow behind car with camera

--- Assets/Scripts/CameraBehaviour.cs
+++ Assets/Scripts/CameraBehaviour.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class CameraBehaviour : MonoBehaviour
 {
-    public GameObject car; 
+    public GameObject car;
+    
+    // variable to save world position transform of our car
+    private Transform target; 
     
     // offset from local point of car to camera position
     private Vector3 offset = new Vector3(0.4f, 1.4f, -5.2f);
@@ -12,13 +15,17 @@ public class CameraBehaviour : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // set the car's transform to the variable target
+        target = car.transform;
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        // set the camera to the position of the car with offset 
-        transform.position = car.transform.position + offset;
+        // set the camera to the position of the car with offset, converted to World Space
+        transform.position = target.TransformPoint(offset);
+        
+        // Look at our car's transform position
+        transform.LookAt(target);
     }
 }
