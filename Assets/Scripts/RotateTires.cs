5d2f547f92cba3933a494485c7a119e69ac26444
Linda Kruse <gitkraken@lindakruse.de>
2020-11-27 12:18:15 +0100

    make tires rotate when driving

---
+++ Assets/Scripts/RotateTires.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateTires : MonoBehaviour
+{
+    public float tireSpeed = 200f;
+    private float verticalInput; 
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        verticalInput = Input.GetAxis("Vertical");
+        
+        if (verticalInput > 0)
+        {
+            transform.Rotate(Vector3.right, Time.deltaTime * tireSpeed * verticalInput, Space.Self);
+        }
+        else if (verticalInput < 0)
+        {
+            transform.Rotate(Vector3.right, Time.deltaTime * tireSpeed * verticalInput, Space.Self);
+        }
+    }
+}
