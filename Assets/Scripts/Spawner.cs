e0065fe8f71707b776738c814f9055339ed1fe96
Linda Kruse <gitkraken@lindakruse.de>
2020-11-27 14:41:52 +0100

    change spawn key

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -15,7 +15,7 @@ public class Spawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.S))
         {
             // launch tire obstacle
             Debug.Log("I did it! I pressed the space bar");
