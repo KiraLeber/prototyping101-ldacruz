13abf3508aed846d9835b17bb730e947cd8cd6b3
Linda Kruse <gitkraken@lindakruse.de>
2020-11-27 14:41:44 +0100

    add ground check

---
+++ Assets/Scripts/HumanController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HumanController : MonoBehaviour
+{
+    private Rigidbody _humanRb;
+    public float jumpForce = 10f;
+    public float gravityModifer;
+    public bool isOnGround = true;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _humanRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifer;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            _humanRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
+    }
+}
