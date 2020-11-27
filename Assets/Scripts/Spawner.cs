e0869f843fa8f34b3179a31ac96266088878cc4c
Linda Kruse <gitkraken@lindakruse.de>
2020-11-27 01:03:04 +0100

    add tire spawner prefab with spawner script
    invoke tire prefab on key down space

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject tire;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // launch tire obstacle
+            Debug.Log("I did it! I pressed the space bar");
+            Instantiate(tire, transform.position, tire.transform.rotation);
+        }
+    }
+}
